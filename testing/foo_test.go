b60610dc5ee2f2b3e07e1a61f0cc3ea20dc47638
Ilnur <ilnrzakirov@gmail.com>
2023-10-08 13:40:14 +0300

    testing

--- testing/foo_test.go
+++ testing/foo_test.go
@@ -4,6 +4,6 @@ import "testing"
 
 func FooTest(t *testing.T) {
 	if actualResult := foo(5, 5); actualResult != 10 {
-		t.Errorf("expected %s, got %s", actualResult, 10)
+		t.Errorf("expected %d, got %d", actualResult, 10)
 	}
 }
